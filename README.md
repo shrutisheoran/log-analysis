08b88074cc3df53c2feb171f0468ade7cf0e2386
Shruti Sheoran <shruti.sheoran03@gmail.com>
2018-01-20 20:02:03 +0530

    Corrected queries

--- README.md
+++ README.md
@@ -1,11 +1,34 @@
 
 # Log Analysis
 
-**_Steps to run file_**
+## This project answers to the following questions using sql
 
+* Most popular articles of all time
+* Most popular authors of all time
+* Date on which more than 1% requests lead to errors
+
+## To know answers follow the instructions
+
+**_Meeting the Requirements_**
 ---
+1. **Configure virtual machine using vagrant**
+* Download vagrant from [here](https://www.vagrantup.com/downloads.html)
+* Download configuration files from [here](https://d17h27t6h515a5.cloudfront.net/topher/2017/August/59822701_fsnd-virtual-machine/fsnd-virtual-machine.zip)
+* Extract those files in a folder then open git bash in the folder.
+* Run `vagarant up` in git bash.
+* Then run `winpty vagrant ssh` or `vagrant ssh` to login.
+* Now you have up and runnig postgresql in your system with python3 and newly created database **_news_**.
 
-1. Open repository
-1. Clone or Download zip file.
-1. Open it in your system.
-1. Run command `python reporting_tool.py` in command prompt.
+2. **Tables and necessary python libraries.**
+* cd into vagrant by running `cd /vagrant`.
+* Run `sudo apt-get install psycopg2` if not installed.
+* To create all the necessary tables download the `newsdata.sql` file from [here](https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip).
+* Then run this command `psql -d news -f newsdata.sql`.
+
+**_Run Log Analysis_**
+---
+1. **Now to obtain the analysis run `python3 reporting_tool.py` or `.\reporting_tool.py`.**
+1. **Open the following text files to see the output.**
+* `popular_articles.txt`
+* `popular_authors.txt`
+* `status.txt`
--- reporting_tool.py
+++ reporting_tool.py
@@ -1,67 +1,75 @@
-#!Python 3.5.2
+#!/usr/bin/env python3
 from datetime import date
 import psycopg2
 
-# This function tells 3 most popular articles of all time.
+''' This function executes queries and fetches result.'''
+
+
+def query_executer(query):
+    conn = psycopg2.connect("dbname=news")
+    cursor = conn.cursor()
+    cursor.execute(query)
+    result = cursor.fetchall()
+    conn.close()
+    return result
+
+''' This function tells 3 most popular articles of all time.'''
 
 
 def popular_articles():
-    conn = psycopg2.connect("dbname=news")
-    cursor = conn.cursor()
-    cursor.execute("select articles.title, views.num from articles\
-, (select path, count(*) as num from log group by path) as \
-views where views.path=concat('/article/', articles.slug) \
-order by num desc; ")
-    result = cursor.fetchall()
+    query = '''select articles.title, views.num from articles, (select path,
+    count(*) as num from log group by path) as views where views.path = concat
+    ('/article/', articles.slug) order by num desc limit 3; '''
+    result = query_executer(query)
     f = open("popular_articles.txt", "a")
+    ''' Writing to popular_articles.txt file.'''
+    f.write('''What are the most popular three articles of all time?\n\n''')
     for el in result:
         formatted_output = str(el[0]) + ' -- ' + str(el[1]) + ' views\n'
-        f.write(str(formatted_output))  # Writing to popular_articles.txt file.
+        f.write(str(formatted_output))
 
     f.close()
-    conn.close()
 
 
-# This function tells most popular article author of all time.
+''' This function tells most popular article author of all time.'''
 
 
 def popular_authors():
-    conn = psycopg2.connect("dbname = news")
-    cursor = conn.cursor()
-    cursor.execute("select authors.name, count(*) as num from log,\
-articles, authors where authors.id=articles.author and log.path=\
-concat('/article/', articles.slug) group by authors.name order \
-by num desc limit 3; ")
-    result = cursor.fetchall()
+    query = '''select authors.name, count(*) as num from log,articles, authors
+     where authors.id=articles.author and log.path = concat
+     ('/article/', articles.slug)
+     group by authors.name order by num desc; '''
+    result = query_executer(query)
     f = open("popular_authors.txt", "a")
+    # Writing to popular_articles.txt file.
+    f.write('''Who are the most popular article authors of all time?\n\n''')
     for el in result:
         formatted_output = str(el[0]) + ' -- ' + str(el[1]) + ' views\n'
-        f.write(str(formatted_output))  # Writing to popular_authors.txt file.
+        f.write(str(formatted_output))
 
     f.close()
-    conn.close()
 
-# This function tells on which day more than 1% of requests lead to errors.
+''' This function tells on which day more than 1% of requests
+ lead to errors.'''
 
 
 def request_error():
-    conn = psycopg2.connect("dbname = news")
-    cursor = conn.cursor()
-    cursor.execute("select error.day, (error.num*100)/cast(ok.num as \
-        float) from (select cast(time as date) as day, count(*) as \
-        num from log where status='404 NOT FOUND' group by day) as \
-        error, (select cast(time as date) as day, count(*) as num \
-        from log group by day) as ok where error.day=ok.day and \
-        (error.num*100)/cast(ok.num as float)>1; ")
-    result = cursor.fetchall()
+    query = '''select error.day, (error.num*100)/cast(ok.num as float) from
+    (select cast(time as date) as day, count(*) as num from log where
+    status='404 NOT FOUND' group by day) as error, (select cast(time as date)
+    as day, count(*) as num from log group by day) as ok where
+    error.day=ok.day and (error.num*100)/cast(ok.num as float)>1; '''
+    result = query_executer(query)
     f = open("status.txt", "a")
+    ''' Writing to popular_articles.txt file.'''
+    f.write('''On which days did more than 1% of requests lead to\
+ errors?\n\n''')
     for el in result:
-        formatted_output = str(el[0]) + ' -- ' + str(round(el[1], 2)) +\
-            '% errors\n'
-        f.write(str(formatted_output))  # Writing to status.txt file.
+        formatted_output = '''{:%B %d, %Y} -- {:.2f}% error\
+        s\n'''.format(el[0], el[1])
+        f.write(str(formatted_output))
 
     f.close()
-    conn.close()
 
 
 popular_articles()
